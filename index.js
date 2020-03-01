cb4f77f15719801931b4dff6840d33dc65a76a47
Kseniya <ksu.xa.v.ya@gmail.com>
2020-03-01 17:00:39 +0700

    connected Calendar Landing-page v.2

--- index.js
+++ index.js
@@ -3,5 +3,4 @@ import '/project/style/header.scss';
 import '/project/style/footer.scss';
 import '/project/style/form.scss';
 import '/project/style/fonts.css';
-
-
+import '/project/style/calendar.scss';
---
+++ project/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$('.calendar').pignoseCalendar({
+		select: function(date, context){
+			console.log(date[0]);
+   		}
+	});
+
+	$('.calendar').pignoseCalendar({
+	  week: 1,
+	  lang: 'ru',
+	  multiple: true,
+	  date: moment('2020-03-05')
+	});
+
+	
+});
+
