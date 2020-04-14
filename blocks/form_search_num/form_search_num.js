cdf5160b04419cf8483a9da33057b5fdc4364f6a
Kseniya <ksu.xa.v.ya@gmail.com>
2020-04-15 00:51:19 +0700

    modif js for calendar in 'langing_page'

--- blocks/form_search_num/form_search_num.js
+++ blocks/form_search_num/form_search_num.js
@@ -3,31 +3,36 @@
 $(function() {
 	$('.form_search_num').find('.block_dropdown .date .expand').on('click', function(e) {
 		var calendar = $(this).closest('.form_search_num').find('.block_calendar');
-		dropdown_expend($(this),calendar);
+		
+		if (calendar.hasClass('hide')){
+			calendar.removeClass('hide');	
+		}else{
+			calendar.addClass('hide');
+		}
 	});	
 
 	$('.form_search_num').find('.filter_guest .expand').on('click', function(e) {
 		var block_list = $(this).closest('.filter_guest').find('.dropdown_items');
+		//expend = $(this).closest('.flex').find('.expend');
 		dropdown_expend($(this),block_list);
+		
+
 	});	
 });
 
 function dropdown_expend(expend,block) {
-	
+	input = expend.closest('.dropdown');
+	//console.log(expend);
 
-	if (block.hasClass('block_calendar')){
-
-	}else{
-		input = expend.closest('.dropdown');
-	}
-
-    if (block.hasClass('hide')){
+	if (block.hasClass('hide')){
+		//console.log(input);
 		block.removeClass('hide');	
 		input.addClass('items_show');
 		expend.css('transform','rotate(180deg)');
 	}else{
+		//console.log('4');
 		block.addClass('hide');
 		input.removeClass('items_show');
 		expend.css('transform','rotate(360deg)');
-	}
+	}    
 }
