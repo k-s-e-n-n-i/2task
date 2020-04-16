1d776b444ffbcc1990bade63f288cb9d03d9db09
Kseniya <ksu.xa.v.ya@gmail.com>
2020-04-17 01:07:57 +0700

    rewrite html to pug, break on block and rename classes scss by BEM

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-//import '/project/style/styles.scss';
+import '/project/style/styles.scss';
 //import '/blocks/header/header.scss';
 //import '/blocks/footer/footer.scss';
 //import '/blocks/form_search_num/form.scss';
 import '/project/style/fonts.css';
 //import '/blocks/calendar/calendar.scss';
+
+import '/ui_kit/form_elements/form_elements.scss'
+import '/ui_kit/form_elements/inputText/inputText.scss'
+import '/ui_kit/form_elements/dropdown/dropdown.scss'
 
