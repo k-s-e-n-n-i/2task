daaff545a74ea11d1a70eb708bb8cd606a32f096
Kseniya <ksu.xa.v.ya@gmail.com>
2020-05-12 01:34:14 +0700

    modif rangeSlider

--- ui_kit/form_elements/rangeSlider/rangeSlider.js
+++ ui_kit/form_elements/rangeSlider/rangeSlider.js
@@ -23,11 +23,12 @@ $(function() {
 	
 
 	//узнаем ширину бегунка, мин и макс значения стоимости
-	let start = -3,
-		width = $('.rangeSlider_slider').width() - $('.rangeSlider_slider .rangeSlider_slider_left').width()-start,
-		min = parseInt($('.rangeSlider .rangeSlider_label__min').html()),
-		max = parseInt($('.rangeSlider .rangeSlider_label__max').html());
+	let width = $('.rangeSlider_slider').width(),
+		min = 0, max = 100000;
 	console.log('width:',width,'\nmin:',min,'\nmax:',max);
+
+	//позиция бегунка
+	const rangePositionLeft = parseInt($('.rangeSlider_slider').position().left);
 
 	//определяем индикатор
 	let ind = $('.rangeSlider_slider .rangeSlider_slider_range');
@@ -62,36 +63,37 @@ $(function() {
 		});
 		
 		function moveAt(e) {
-			pos = e.pageX - range.width() / 2 -80;
+			pos = e.pageX - rangePositionLeft; // 
 
-			console.log('fvffgfv',e.pageX);
+			console.log(pos);
 
-			if ( (pos >= start) && (pos <= width) ){
-				console.log(pos);
-			
+			if ((pos >= 0) && (pos <= width)){
+				
 
 				range.css('left', pos+'px');//e.pageX - range.offsetWidth / 2 + 'px'; //позиция указателей
 
-				pr = (pos-start) / width,
+				//return true; // в этот
+				pr = pos / width,
 				price = ((max - min) * pr + min).toFixed();
 				//console.log(price+'rub')
 
 
-				
+
 				if (cl == 'left'){
 					step = startPos - parseInt(range.css('left'));//длина перемещения левого указателя
-					ind.css('left',e.pageX - range.width() / 2 -78+ 'px');//позиция индикатора
+					ind.css('transform','translate('+pos+'px, -1px)');
+					//ind.css('left',e.pageX - rangePositionLeft+ 'px');//позиция индикатора
 					range.closest('.rangeSlider').find('.rangeSlider_label__min').html(price);
 				}
 				if (cl == 'right'){
 					step = parseInt(range.css('left')) - startPos;//длина перемещения правого указателя
 					range.closest('.rangeSlider').find('.rangeSlider_label__max').html(price);
 				}
-				
+
 				//console.log('?',startPos , parseInt(range.css('left')),step);
 				ind.css('width', indWidth+step+'px');//ширина индикатора
+
 			}
-				
 		}
 	}
 	function count(range){
