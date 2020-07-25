ce481dd9939c6a80efde7a433388afbffba2eded
unknown <ksu.xa.v.ya@gmail.com>
2020-07-25 23:01:42 +0700

    slider

--- ui_kit/form_elements/rangeSlider/rangeSlider.js
+++ ui_kit/form_elements/rangeSlider/rangeSlider.js
@@ -1,110 +1,146 @@
 $(function() {
-
-
-
-	/*$('.rangeSlider .rangeSlider_range_left').on('click', function(e) {
-		let min = 0, max = 10000,
-			rangeSlider = $(this).closest('.rangeSlider'),
-			rangeSlider_range_left = $(this).closest('.rangeSlider_range'),
-
-			widthStrLen = rangeSlider.css('width').length,
-			leftStrLen = rangeSlider_range_left.css('left').length,
-
-			width = Number(rangeSlider.css('width').slice(0,widthStrLen-2)),
-			left = Number(rangeSlider_range_left.css('left').slice(0,leftStrLen-2)),
-			pr = left / width * 100;
-
-
-
-			console.log('min: '+min+'\nmax: '+max+'\nwidth: '+width+'\nleft: '+left+'\n\npr: '+pr+'\n\n');
-		//range($(this).closest('.rangeSlider_range'));
-	});*/
-
-	
-
-	//узнаем ширину бегунка, мин и макс значения стоимости
-	let width = $('.rangeSlider_slider').width(),
-		min = 0, max = 100000;
-	console.log('width:',width,'\nmin:',min,'\nmax:',max);
-
-	//позиция бегунка
-	const rangePositionLeft = parseInt($('.rangeSlider_slider').position().left);
-
-	//определяем индикатор
-	let ind = $('.rangeSlider_slider .rangeSlider_slider_range');
-
-	//смена левой границы
-	let rangeLeft = $('.rangeSlider_slider .rangeSlider_slider_left');
-	rangeLeft.on('mousedown', function(e) {
-		movie(rangeLeft,e,'left');	
-	});
-
-	//смена правой границы
-	let rangeRight = $('.rangeSlider_slider .rangeSlider_slider_right');
-	rangeRight.on('mousedown', function(e) {
-		movie(rangeRight,e,'right');		
-	});
-	
-
-	function movie(range,e,cl){
-		startPos = parseInt(range.css('left'));
-		indWidth = ind.width();
-
-		moveAt(e);
-	
-		$(document).on('mousemove', function(e) {
-	  		moveAt(e);
-		});
-
-		$(document).on('mouseup', function(e) {
-	  		$(document).off('mousemove');
-	  		$(document).off('mouseup');
-	  		//count(range);
-		});
-		
-		function moveAt(e) {
-			pos = e.pageX - rangePositionLeft; // 
-
-			console.log(pos);
-
-			if ((pos >= 0) && (pos <= width)){
-				
-
-				range.css('left', pos+'px');//e.pageX - range.offsetWidth / 2 + 'px'; //позиция указателей
-
-				//return true; // в этот
-				pr = pos / width,
-				price = ((max - min) * pr + min).toFixed();
-				//console.log(price+'rub')
-
-
-
-				if (cl == 'left'){
-					step = startPos - parseInt(range.css('left'));//длина перемещения левого указателя
-					ind.css('transform','translate('+pos+'px, -1px)');
-					//ind.css('left',e.pageX - rangePositionLeft+ 'px');//позиция индикатора
-					range.closest('.rangeSlider').find('.rangeSlider_label__min').html(price);
-				}
-				if (cl == 'right'){
-					step = parseInt(range.css('left')) - startPos;//длина перемещения правого указателя
-					range.closest('.rangeSlider').find('.rangeSlider_label__max').html(price);
-				}
-
-				//console.log('?',startPos , parseInt(range.css('left')),step);
-				ind.css('width', indWidth+step+'px');//ширина индикатора
-
-			}
-		}
-	}
-	function count(range){
-		let pos = parseInt(range.css('left')),
-			pr = pos / width,
-			price = (max - min) * pr + min;
-
-			console.log(price+'rub')
-	
-	}
+	$('body').slider();
 
 
 });
 
+
+
+(function( $ ) {
+$.fn.slider = function() {
+
+		let dataSlider = {
+			min : 0,
+			max : 500000,
+			width : 256,
+			idElement : 'idPrice',
+			step : 'no'
+		}
+
+		let dataSlider2 = {
+			min : 0,
+			max : 10,
+			width : 100,
+			idElement : 'idPrice2',
+			step : 5
+		}
+
+		run(dataSlider);
+		run(dataSlider2);
+
+
+		function run(obj) {
+
+			let sliderBlock = $('.rangeSlider#'+obj.idElement),
+				slider = sliderBlock.find('.rangeSlider_slider'),
+				ind = slider.find('.rangeSlider_slider_range'),//"индикатор"
+				width = obj.width,
+				min = obj.min, 
+				max = obj.max,
+				rangePositionLeft = parseInt(slider.position().left);//позиция бегунка
+				
+			console.log('width:',width,'\nmin:',min,'\nmax:',max);
+
+			sliderBlock.find('.rangeSlider_label__min').html(min);
+			sliderBlock.find('.rangeSlider_label__max').html(max);
+			slider.css('width',obj.width);
+
+
+			//смена левой границы
+			let rangeLeft = slider.find('.rangeSlider_slider_left');
+			rangeLeft.on('mousedown', function(e) {
+				movie(rangeLeft,e,'left');	
+			});
+
+			//смена правой границы
+			let rangeRight = slider.find('.rangeSlider_slider_right');
+			rangeRight.on('mousedown', function(e) {
+				movie(rangeRight,e,'right');		
+			});
+			
+
+			function movie(range,e,cl){
+				let startPos = parseInt(range.css('left')),
+					indWidth = ind.width();
+
+				moveAt(e);
+			
+				$(document).on('mousemove', function(e) {
+			  		moveAt(e);
+				});
+
+				$(document).on('mouseup', function(e) {
+			  		$(document).off('mousemove');
+			  		$(document).off('mouseup');
+				});
+				
+				function moveAt(e) {
+					pos = e.pageX - rangePositionLeft;
+
+					if ((pos >= 0) && (pos <= width)){
+						
+
+
+						if (cl == 'left'){
+							rigth = parseInt(rangeRight.css('left'));
+							if (rigth >= pos){
+								price = calc(range);
+
+								if (obj.step == 'no'){
+									step = startPos - parseInt(range.css('left'));//длина перемещения левого указателя	
+									console.log('step:',step);
+									ind.css('transform','translate('+pos+'px, -1px)');
+									range.closest('.rangeSlider').find('.rangeSlider_label__min').html(price);
+								}else{
+									step = startPos - parseInt(range.css('left'));
+									//step = steped(step);
+
+									//console.log('step:',step,'\npos:',pos);
+									//pos = rounding(pos,obj.step);
+
+									ind.css('transform','translate('+pos+'px, -1px)');
+									range.closest('.rangeSlider').find('.rangeSlider_label__min').html(price);
+								}
+								
+								
+							}
+						}
+
+						if (cl == 'right'){
+							left = parseInt(rangeLeft.css('left'));
+							if (left <= pos){
+								price = calc(range);
+
+								step = parseInt(range.css('left')) - startPos;//длина перемещения правого указателя
+								range.closest('.rangeSlider').find('.rangeSlider_label__max').html(price);
+							}
+						}
+
+						ind.css('width', indWidth+step+'px');//ширина индикатора
+					}
+
+					function calc(range){
+						range.css('left', pos+'px');//позиция указателей
+
+						pr = pos / width,
+						price = ((max - min) * pr + min).toFixed();
+						return price;
+					}
+
+					function steped(step){
+						console.log('func'+(step/obj.step).toFixed()*obj.step);
+					}
+
+					function rounding(num,step){
+						pos = (num/step).toFixed()*step;
+						console.log('check',pos);
+					}
+				}
+			}
+
+			
+
+		}
+
+	};
+})(jQuery);
