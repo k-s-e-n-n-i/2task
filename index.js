e77bd69aaf88016f491c994532f88cbb0b0a632f
KsuVla <ksu.xa.v.ya@gmail.com>
2021-04-11 20:23:00 +0700

    edit folders (toxin-4)

--- index.js
+++ index.js
@@ -1,52 +1,55 @@
 //import $ from 'jquery'
 import '/project/font/fonts.css'
-import '/project/style/styles.scss'
+import '/project/styles/styles.scss'
 
 /*form_elements*/
-import '/ui_kit/form_elements/form_elements.scss'
-import '/ui_kit/form_elements/inputText/inputText.scss'
-import '/ui_kit/form_elements/dropdown/dropdown.scss'
-import '/ui_kit/form_elements/dropdown/dropdown.js'
-import '/ui_kit/form_elements/btn/btn.scss'
-import '/ui_kit/form_elements/checkbox/checkbox.scss'
-import '/ui_kit/form_elements/checkbox/checkboxList.js'
-import '/ui_kit/form_elements/radiobutton/radiobutton.scss'
-import '/ui_kit/form_elements/toggle/toggle.scss'
-import '/ui_kit/form_elements/toggle/toggle.js'
-import '/ui_kit/form_elements/like/like.scss'
-import '/ui_kit/form_elements/like/like.js'
-import '/ui_kit/form_elements/rateBtn/rateBtn.scss'
-import '/ui_kit/form_elements/rateBtn/rateBtn.js'
-import '/ui_kit/form_elements/rangeSlider/rangeSlider.scss'
-import '/ui_kit/form_elements/link/link.scss'
-import '/ui_kit/form_elements/pagination/pagination.scss'
-import '/ui_kit/form_elements/list/list.scss'
-import '/ui_kit/form_elements/info/info.scss'
-import '/ui_kit/form_elements/comment/comment.scss'
+import '/blocks/elements/inputText/inputText.scss'
+import '/blocks/elements/dropdown/dropdown.scss'
+import '/blocks/elements/dropdown/dropdown.js'
+import '/blocks/elements/btn/btn.scss'
+import '/blocks/elements/checkbox/checkbox.scss'
+import '/blocks/elements/checkbox/checkboxList.js'
+import '/blocks/elements/radiobutton/radiobutton.scss'
+import '/blocks/elements/toggle/toggle.scss'
+import '/blocks/elements/toggle/toggle.js'
+import '/blocks/elements/like/like.scss'
+import '/blocks/elements/like/like.js'
+import '/blocks/elements/rateBtn/rateBtn.scss'
+import '/blocks/elements/rateBtn/rateBtn.js'
+import '/blocks/elements/rangeSlider/rangeSlider.scss'
+import '/blocks/elements/link/link.scss'
+import '/blocks/elements/pagination/pagination.scss'
+import '/blocks/elements/list/list.scss'
+import '/blocks/elements/info/info.scss'
+import '/blocks/elements/comment/comment.scss'
 
 
 /*color_type*/
-import '/ui_kit/color_type/color_type.scss'
+import '/pages/ui-kit_colorType/color_type.scss'
 
 
 /*header_footer*/
-import '/ui_kit/header_footer/header.scss'
-import '/ui_kit/header_footer/footer.scss'
-import '/ui_kit/header_footer/header_footer.scss'
+import '/pages/ui-kit_headerFooter/header.scss'
+import '/pages/ui-kit_headerFooter/footer.scss'
+import '/pages/ui-kit_headerFooter/header_footer.scss'
 
 
 /*cards*/
-import '/ui_kit/cards/cards.scss'
-import '/ui_kit/cards/formSearchNum/formSearchNum.scss'
-import '/ui_kit/cards/formSearchNum/formSearchNum.js'
-import '/ui_kit/cards/calendar/calendar.scss'
-//import '/ui_kit/cards/calendar/calendar.js'
-import '/ui_kit/cards/formRegistration/formRegistration.scss'
-import '/ui_kit/cards/formReservation/formReservation.scss'
-import '/ui_kit/cards/formReservation/formReservation.js'
-import '/ui_kit/cards/formSignIn/formSignIn.scss'
-import '/ui_kit/cards/cardRoom/cardRoom.scss'
-import '/ui_kit/cards/cardRoom/cardRoom.js'
+import '/pages/ui-kit_cards/cards.scss'
+import '/blocks/cards/formSearchNum/formSearchNum.scss'
+import '/blocks/cards/formSearchNum/formSearchNum.js'
+import '/blocks/cards/calendar/calendar.scss'
+//import '/blocks/cards/calendar/calendar.js'
+import '/blocks/cards/formRegistration/formRegistration.scss'
+import '/blocks/cards/formReservation/formReservation.scss'
+import '/blocks/cards/formReservation/formReservation.js'
+import '/blocks/cards/formSignIn/formSignIn.scss'
+import '/blocks/cards/cardRoom/cardRoom.scss'
+import '/blocks/cards/cardRoom/cardRoom.js'
+
+
+/*form elements*/
+import '/pages/ui-kit_formElements/form_elements.scss'
 
 
 /*pages*/
