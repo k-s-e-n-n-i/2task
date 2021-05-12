abaf59824845ea47c88b1809f2514f0e4d7351fc
KsuVla <ksu.xa.v.ya@gmail.com>
2021-05-12 17:40:45 +0700

    build project

---
+++ docs/ui-kit_colorType.f4793f14.js
@@ -0,0 +1,10 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"ogqL":[function(require,module,exports) {
+
+},{"./montserrat\\MontserratRegular\\MontserratRegular.eot":[["MontserratRegular.e6d05207.eot","J3mv"],"J3mv"],"./montserrat\\MontserratRegular\\MontserratRegular.woff":[["MontserratRegular.fc511161.woff","ff1T"],"ff1T"],"./montserrat\\MontserratRegular\\MontserratRegular.ttf":[["MontserratRegular.d0760cfd.ttf","qerS"],"qerS"]}],"CU9K":[function(require,module,exports) {
+
+},{}],"wZ8E":[function(require,module,exports) {
+
+},{}],"g3Ow":[function(require,module,exports) {
+"use strict";require("/project/font/fonts.css"),require("/project/styles/styles.scss"),require("/pages/ui-kit_colorType/ui-kit_colorType.scss");
+},{"/project/font/fonts.css":"ogqL","/project/styles/styles.scss":"CU9K","/pages/ui-kit_colorType/ui-kit_colorType.scss":"wZ8E"}]},{},["g3Ow"], null)
+//# sourceMappingURL=/2task/ui-kit_colorType.f4793f14.js.map
