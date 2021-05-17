3a3961c0b31eadd85c715b15c468c67f3535d421
KsuVla <ksu.xa.v.ya@gmail.com>
2021-05-17 17:53:17 +0700

    fixed toggle-block.js

--- src/blocks/elements/toggle-block/toggle-block.js
+++ src/blocks/elements/toggle-block/toggle-block.js
@@ -1,13 +1,13 @@
 import $ from "jquery"
 
 $(function() {
-  $('.toggle-block__input-label').on('click', handleToggleClick)
+  $('.toggle-block__input-label').on('click', handleToggleClick);
 
   function handleToggleClick(){
-    if ($(this).hasClass('off')){
+    if ($(this).hasClass('toggle-block__input-label_off')){
       $(this).closest('.toggle-block__toggle').css('border-color','rgba(31, 32, 65, 0.25)');
     }
-    if ($(this).hasClass('on')){
+    if ($(this).hasClass('toggle-block__input-label_on')){
       $(this).closest('.toggle-block__toggle').css('border-color','#BC9CFF');
     }
   }
