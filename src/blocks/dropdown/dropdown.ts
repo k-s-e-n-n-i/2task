a568a69fc8357a3dfe7f916907ff442a94fda1fa
KsuVla <ksu.xa.v.ya@gmail.com>
2021-05-28 19:40:13 +0700

    fixed dropdown - printing counted qty in dropdown (#5)

--- src/blocks/dropdown/dropdown.ts
+++ src/blocks/dropdown/dropdown.ts
@@ -6,6 +6,9 @@ $(function() {
   $('.dropdown-block__inc-qty-minus').on('click', handleDropdownMinusClick);
   $('.dropdown-block__inc-qty-plus').on('click', handleDropdownPlusClick);
 
+  $('.dropdown-block__btns .link_ok').on('click', handleDropdownOkClick);
+  $('.dropdown-block__btns .link_clean').on('click', handleDropdownCleanClick);
+
 
   function handleDropwownClick(){
     var block_list = $(this).closest('.dropdown-block').find('.dropdown-block__dropdown-items');
@@ -43,6 +46,8 @@ $(function() {
         plus.removeClass('dropdown-block__inc-qty_disable');
       }
     }
+
+    outputInDropdown($(this).closest('.dropdown-block'));
   }
 
   function handleDropdownPlusClick(){
@@ -68,6 +73,96 @@ $(function() {
         minus.removeClass('dropdown-block__inc-qty_disable');
       }
     }
+
+    outputInDropdown($(this).closest('.dropdown-block'));
+  }
+
+
+  function handleDropdownOkClick(){
+    event.preventDefault();
+    $(this).closest('.dropdown-block').find('.dropdown-block__dropdown').click();
+    
+    outputInDropdown($(this).closest('.dropdown-block'));
+  }
+
+  function handleDropdownCleanClick() {
+    event.preventDefault();
+    let items = $(this).closest('.dropdown-block__dropdown-items');
+    items.find('.dropdown-block__block-qty').find('span').html('0');
+    items.find('.dropdown-block__inc-qty-minus').addClass('dropdown-block__inc-qty_disable');
+    
+    outputInDropdown($(this).closest('.dropdown-block'));
+  }
+
+
+  function outputInDropdown(dropdown : any) {
+    let str : string = '';
+
+    if (dropdown.attr('name') == 'guests'){
+      str = countQtyGuests(dropdown);
+    }
+    if (dropdown.attr('name') == 'room'){
+      str = countQtyComfortRoom(dropdown);
+    }
+
+    if (str.length > 19){
+      str = str.substr(0, 20) + '...';
+    }
+    dropdown.find('.dropdown-block__dropdown').html(str);
+  }
+
+  function countQtyGuests(dropdown : any) : string {
+    let lines = dropdown.find('.dropdown-block__items-line'),
+        str : string = '',
+        sumGuests : number = 0,
+        sumBaby : number = 0,
+        qty : HTMLElement,
+        item : HTMLElement;
+    
+    for(let i = 0; i < lines.length; i++){
+      qty = lines[i].querySelector('.dropdown-block__block-qty span');
+      item = lines[i].querySelector('h3');
+      if ((parseInt(qty.innerHTML) > 0) && (item.innerHTML != 'младенцы')){
+        sumGuests = sumGuests + parseInt(qty.innerHTML);
+      }
+      if ((parseInt(qty.innerHTML) > 0) && (item.innerHTML == 'младенцы')){
+        sumBaby = sumBaby + parseInt(qty.innerHTML);
+      }
+    }
+
+    if (sumGuests == 0){
+      str = 'Сколько гостей';
+    }
+    if ((sumGuests != 0) && (sumBaby != 0)){
+      str = sumGuests + ' гостя, ' + sumBaby + ' младенец';
+    }
+    if ((sumGuests != 0) && (sumBaby == 0)){
+      str = sumGuests + ' гостя';
+    }
+
+    return str;
+  }
+
+  function countQtyComfortRoom(dropdown : any) : string {
+    let lines = dropdown.find('.dropdown-block__items-line'),
+        str : string = '',
+        qty : HTMLElement,
+        item : HTMLElement;
+    
+    for(let i = 0; i < lines.length; i++){
+      qty = lines[i].querySelector('.dropdown-block__block-qty span');
+      item = lines[i].querySelector('h3');
+      if (parseInt(qty.innerHTML) > 0){
+        str = str + qty.innerHTML + ' ' + item.innerHTML + ', ';
+      }
+    }
+
+    str = str.substr(0, str.length-2);
+    if (str == ''){
+      str = 'Выберите удобства';
+    }
+
+    return str;
   }
   
 });
