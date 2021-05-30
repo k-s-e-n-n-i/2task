3a79123ab64cedb7383438939d7073daae4133f9
KsuVla <ksu.xa.v.ya@gmail.com>
2021-05-31 03:46:24 +0700

    add closeCalendar(), apply, select (#5)

--- src/blocks/calendar/calendar.js
+++ src/blocks/calendar/calendar.js
@@ -1,15 +1,46 @@
 import 'pg-calendar';
 
 $(document).ready(function() {
-  $('.calendar').pignoseCalendar({
-    week: 1,
-    lang: 'ru',
-    multiple: true,
-    buttons: true,
+  $('.calendar').pignoseCalendar({     
+      week: 1,
+      lang: 'ru',
+      multiple: true,
+      buttons: true,
+      format: 'DD.MM.YYYY',
+      controls: {
+        ru: {
+          cancel: "Очистить",
+          ok: "Применить"
+        }
+      },
+      apply: function(date, context) {
+        
+
+        
+        $(this).closest('.dropdown-dates').find('.dropdown-block__dropdown:first').trigger('click');
+      },
+      select: function(date, context) {
+        console.log(context);//, context.context.settings.format)
+
+        console.log(date[0], date[1]);
+        //console.log(date[0]._i, date[1]._i);
+
+        console.log(date[0], date[1]);
+        if ((date[0] != null) && (date[0]._i != undefined)){
+          $(this).closest('.dropdown-dates').find('.dropdown-block__dropdown')[0].innerText = date[0]._i;
+        }else{
+          $(this).closest('.dropdown-dates').find('.dropdown-block__dropdown')[0].innerText = 'ДД.ММ.ГГГГ';
+        }
+        if ((date[1] != null) && (date[1]._i != undefined)){
+          $(this).closest('.dropdown-dates').find('.dropdown-block__dropdown')[1].innerText = date[1]._i;
+        }else{
+          $(this).closest('.dropdown-dates').find('.dropdown-block__dropdown')[1].innerText = 'ДД.ММ.ГГГГ';
+        }
+      }
   });
 
-  $('.pignose-calendar-button-cancel').html('Очистить');
-  $('.pignose-calendar-button-apply').html('Применить');
+  //$('.pignose-calendar-button-cancel').html('Очистить');
+  //$('.pignose-calendar-button-apply').html('Применить');
 });
 
 
@@ -28,4 +59,35 @@ $(function() {
       $(this).removeClass('dropdown-block__dropdown_items-show');
     }
   }
+
+
+  //$('.pignose-calendar-button-apply').on('click', handleDropdownOkClick);
+  $('.pignose-calendar-button-cancel').on('click', handleDropdownCleanClick);
+
+  /*function handleDropdownOkClick(e){
+    e.preventDefault();
+    $(this).closest('.dropdown-dates').find('.dropdown-block__dropdown:first').trigger('click');
+  }*/
+
+  function handleDropdownCleanClick(e){
+    e.preventDefault();
+  }
+
+
+  $(document).on('click', closeCalendar);
+
+  function closeCalendar(e){
+    const thisClick = $(e.target),
+        elDropdown = thisClick.hasClass('dropdown-block__dropdown') == true,
+        open = !$('.calendar-block').hasClass('calendar-block_hide');
+
+    if (!elDropdown && open){
+      const itCalendar = ((thisClick.closest('.calendar-block').length != 0) || (thisClick.hasClass('calendar-block')));
+
+      if (!itCalendar){
+        $('.dropdown-dates .dropdown-block__dropdown:first').trigger('click');
+      }
+    }    
+  }
+  
 });
