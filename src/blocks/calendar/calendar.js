b8744f0adad0a2c80fd8a4143ad3f7d0f5dfe956
KsuVla <ksu.xa.v.ya@gmail.com>
2021-06-01 02:24:11 +0700

    fixed calendar - closed calendar/dropdown with error (#5)

--- src/blocks/calendar/calendar.js
+++ src/blocks/calendar/calendar.js
@@ -1,148 +1,132 @@
 import 'pg-calendar';
 
 $(function() {
-  $('.dropdown-dates .calendar').pignoseCalendar({     
-      week: 1,
-      lang: 'custom',
-      multiple: true,
-      buttons: true,
-      format: 'DD.MM.YYYY',
-      weeks: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
-      monthsLong: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-      months: ['Янв', 'Февр', 'Март', 'Апр', 'Май', 'Июнь', 'Июль', 'Авг', 'Сент', 'Окт', 'Нояб', 'Дек'],
-      controls: {
-        cancel: "Очистить",
-        ok: "Применить"
-      },
-      
-      apply: function(date, context) { 
-        $(this).closest('.dropdown-dates').find('.dropdown-block__dropdown:first').trigger('click');
-      },
-
-      select: function(date, context) {
-        console.log(context.context.settings.controls.ok, date);
-
-        if ((date[0] != null) && (date[0]._i != undefined)){
-          $(this).closest('.dropdown-dates').find('.dropdown-block__dropdown')[0].innerText = date[0].format('DD.MM.YYYY');
-        }else{
-          $(this).closest('.dropdown-dates').find('.dropdown-block__dropdown')[0].innerText = 'ДД.ММ.ГГГГ';
-        }
-        if ((date[1] != null) && (date[1]._i != undefined)){
-          $(this).closest('.dropdown-dates').find('.dropdown-block__dropdown')[1].innerText = date[1].format('DD.MM.YYYY');
-        }else{
-          $(this).closest('.dropdown-dates').find('.dropdown-block__dropdown')[1].innerText = 'ДД.ММ.ГГГГ';
-        }
-      }
-  });
-
-
-  $('.dropdown-dates .dropdown-block__dropdown').on('click', handleCalendarblockClick);
-
-  function handleCalendarblockClick(){
-    const block_calendar = $(this).closest('.dropdown-dates').find('.calendar-block');
-
-    if (block_calendar.hasClass('calendar-block_hide')){
-      block_calendar.removeClass('calendar-block_hide');
-      $(this).addClass('dropdown-block__dropdown_items-show');
-    }else{
-      block_calendar.addClass('calendar-block_hide');
-      $(this).removeClass('dropdown-block__dropdown_items-show');
-    }
-  }
-
-
-  $('.pignose-calendar-button-cancel').on('click', handleDropdownCleanClick);
-
-  function handleDropdownCleanClick(e){
-    e.preventDefault();
-    $(this).closest('.dropdown-dates').find('.dropdown-block__dropdown')[0].innerText = 'ДД.ММ.ГГГГ';
-    $(this).closest('.dropdown-dates').find('.dropdown-block__dropdown')[1].innerText = 'ДД.ММ.ГГГГ';
-    $(this).closest('.dropdown-dates').find('.dropdown-block__dropdown:first').trigger('click');
-  }
-
-
-  $(document).on('click', closeCalendar);
-
-  function closeCalendar(e){
-    const thisClick = $(e.target),
-        elDropdown = thisClick.hasClass('dropdown-block__dropdown') == true,
-        open = !$('.dropdown-dates .calendar-block').hasClass('calendar-block_hide');
-
-    if (!elDropdown && open){
-      const itCalendar = ((thisClick.closest('.calendar-block').length != 0) || (thisClick.hasClass('calendar-block')));
-
-      if (!itCalendar){
-        $('.dropdown-dates .dropdown-block__dropdown:first').trigger('click');
-      }
-    }    
-  }
-  
-});
-
-
-$(function() {
-  $('.dropdown-block .calendar').pignoseCalendar({     
+  const mainOptions = {
     week: 1,
-    lang: 'ru',
+    lang: 'custom',
     multiple: true,
     buttons: true,
     format: 'DD.MM.YYYY',
-
-    apply: function(date, context) { 
-      $(this).closest('.dropdown-block').find('.dropdown-block__dropdown').trigger('click');
+    weeks: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+    monthsLong: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+    months: ['Янв', 'Февр', 'Март', 'Апр', 'Май', 'Июнь', 'Июль', 'Авг', 'Сент', 'Окт', 'Нояб', 'Дек'],
+    controls: {
+      cancel: "Очистить",
+      ok: "Применить"
     },
+  }
 
-    select: function(date, context) {
-      if ((date[0] != null) && (date[0]._i != undefined) && (date[1] != null) && (date[1]._i != undefined)){
-        const month1 = context.context.settings.months[date[0].format('M')-1];
-        const month2 = context.context.settings.months[date[1].format('M')-1];
-        
-        $(this).closest('.dropdown-block').find('.dropdown-block__dropdown').html(`${date[0].format('D')} ${month1} - ${date[1].format('D')} ${month2}`);
-      }else{
-        $(this).closest('.dropdown-block').find('.dropdown-block__dropdown').html('Выберите даты...');
+  $('.dropdown-dates .calendar').pignoseCalendar(
+    $.extend(
+      mainOptions,
+      { 
+        apply: function(date, context) { 
+          $(this).closest('.dropdown-dates').find('.dropdown-block__dropdown:first').trigger('click');
+        },
+
+        select: function(date, context) {
+          if (date[0].format() != null){
+            $(this).closest('.dropdown-dates').find('.dropdown-block__dropdown')[0].innerText = date[0].format('DD.MM.YYYY');
+          }else{
+            $(this).closest('.dropdown-dates').find('.dropdown-block__dropdown')[0].innerText = 'ДД.ММ.ГГГГ';
+          }
+          if (date[1] != null){
+            $(this).closest('.dropdown-dates').find('.dropdown-block__dropdown')[1].innerText = date[1].format('DD.MM.YYYY');
+          }else{
+            $(this).closest('.dropdown-dates').find('.dropdown-block__dropdown')[1].innerText = 'ДД.ММ.ГГГГ';
+          }
+        }
+      }
+    )
+  );
+
+  //------------------------------------------------------------------------------------
+
+  $('.dropdown-block .calendar').pignoseCalendar(
+    $.extend(
+      mainOptions,
+      {
+        apply: function(date, context) { 
+          $(this).closest('.dropdown-block').find('.dropdown-block__dropdown').trigger('click');
+        },
+
+        select: function(date, context) {
+          if ((date[0].format() != null) && (date[1] != null)){
+            const month1 = context.context.settings.months[date[0].format('M')-1];
+            const month2 = context.context.settings.months[date[1].format('M')-1];
+
+            $(this).closest('.dropdown-block').find('.dropdown-block__dropdown').html(`${date[0].format('D')} ${month1} - ${date[1].format('D')} ${month2}`);
+          }else{
+            $(this).closest('.dropdown-block').find('.dropdown-block__dropdown').html('Выберите диапазон дат...');
+          }
+        }
+      }
+    )
+  );
+
+  //------------------------------------------------------------------------------------
+
+
+  $('.dropdown-block__dropdown').on('click', handleDateDropdownClick);
+  
+  function handleDateDropdownClick(){
+    const block_calendar;
+
+    if ($(this).closest('.dropdown-dates').length != 0){
+      block_calendar = $(this).closest('.dropdown-dates').find('.calendar-block');
+    }else{
+      if ($(this).closest('.dropdown-block').length != 0){
+        block_calendar = $(this).closest('.dropdown-block').find('.calendar-block');
       }
     }
-  });
-
-
-  $('.dropdown-block .dropdown-block__dropdown').on('click', handleCalendarblockClick);
-
-  function handleCalendarblockClick(){
-    const block_calendar = $(this).closest('.dropdown-block').find('.calendar-block');
 
     if (block_calendar.hasClass('calendar-block_hide')){
-      block_calendar.removeClass('calendar-block_hide');
+      block_calendar.removeClass('calendar-block_hide').addClass('calendar-block_open');
       $(this).addClass('dropdown-block__dropdown_items-show');
     }else{
-      block_calendar.addClass('calendar-block_hide');
+      block_calendar.addClass('calendar-block_hide').removeClass('calendar-block_open');
       $(this).removeClass('dropdown-block__dropdown_items-show');
     }
+    
   }
 
 
-  $('.pignose-calendar-button-cancel').on('click', handleDropdownCleanClick);
+  $('.pignose-calendar-button-cancel').on('click', handleCalendarCleanClick);
 
-  function handleDropdownCleanClick(e){
+  function handleCalendarCleanClick(e){
     e.preventDefault();
-    $(this).closest('.dropdown-block').find('.dropdown-block__dropdown').html('Выберите даты...');
-    $(this).closest('.dropdown-block').find('.dropdown-block__dropdown').trigger('click');
+    if ($(this).closest('.dropdown-dates').length != 0){
+      $(this).closest('.dropdown-dates').find('.dropdown-block__dropdown')[0].innerText = 'ДД.ММ.ГГГГ';
+      $(this).closest('.dropdown-dates').find('.dropdown-block__dropdown')[1].innerText = 'ДД.ММ.ГГГГ';
+      $(this).closest('.dropdown-dates').find('.dropdown-block__dropdown:first').trigger('click');
+    }else{
+      if ($(this).closest('.dropdown-block').length != 0){
+        $(this).closest('.dropdown-block').find('.dropdown-block__dropdown').html('Выберите диапазон дат...');
+        $(this).closest('.dropdown-block').find('.dropdown-block__dropdown').trigger('click');
+      }
+    }
   }
 
 
   $(document).on('click', closeCalendar);
 
   function closeCalendar(e){
-    const thisClick = $(e.target),
-        elDropdown = thisClick.hasClass('dropdown-block__dropdown') == true,
-        open = !$('.dropdown-block .calendar-block').hasClass('calendar-block_hide');
-
-    console.log(open)
+    const thisClick = $(e.target);
+    const elDropdown = thisClick.hasClass('dropdown-block__dropdown') == true;
+    const open = $('.calendar-block').hasClass('calendar-block_open');
+    
     if (!elDropdown && open){
-      const itCalendar = ((thisClick.closest('.calendar-block').length != 0) || (thisClick.hasClass('calendar-block')));
+      const itCalendar = ((thisClick.closest('.calendar-block_open').length != 0) || (thisClick.hasClass('calendar-block_open')));
 
-      if (!itCalendar){
-        $('.dropdown-block .dropdown-block__dropdown').trigger('click');
+      if (!itCalendar){      
+        if ($('.calendar-block_open').closest('.dropdown-dates').length != 0){
+          $('.calendar-block_open').closest('.dropdown-dates').find('.dropdown-block__dropdown:first').trigger('click');
+        }
+        if ($('.calendar-block_open').closest('.dropdown-block').length != 0){
+          $('.calendar-block_open').closest('.dropdown-block').find('.dropdown-block__dropdown').trigger('click');
+        }
       }
     }    
   }
+
 });
