8036219dc66d109681acc3f8fcf1c46400d7733c
KsuVla <ksu.xa.v.ya@gmail.com>
2021-06-11 23:49:58 +0700

    fixed search-room by styles (#5)

--- src/pages/search-room/search-room.js
+++ src/pages/search-room/search-room.js
@@ -1,29 +1,28 @@
-import '../../font/fonts.css'
-import '../../index.scss'
-
+import '../../font/fonts.css';
+import '../../index.scss';
 
 /*blocks*/
-import '../../blocks/btn/btn.scss'
-import '../../blocks/calendar/calendar.scss'
-import '../../blocks/calendar/calendar.js'
-import '../../blocks/card-room/card-room.scss'
-import '../../blocks/card-room/card-room.js'
-import '../../blocks/checkbox-list/checkbox-list.scss'
-import '../../blocks/checkbox-list-expandable/checkbox-list-expandable.scss'
-import '../../blocks/checkbox-list-expandable/checkbox-list-expandable.js'
-import '../../blocks/dropdown/dropdown.scss'
-import '../../blocks/dropdown/dropdown.ts'
-import '../../blocks/footer/footer.scss'
-import '../../blocks/header/header.scss'
-import '../../blocks/input-text/input-text.scss'
-import '../../blocks/link/link.scss'
-import '../../blocks/pagination/pagination.scss'
-import '../../blocks/range-slider/range-slider.scss'
-import '../../blocks/rate-btn/rate-btn.scss'
-import '../../blocks/rate-btn/rate-btn.js'
-import '../../blocks/topic-label/topic-label.scss'
+import '../../blocks/btn/btn.scss';
+import '../../blocks/checkbox-list/checkbox-list.scss';
+import '../../blocks/checkbox-list-expandable/checkbox-list-expandable.scss';
+import '../../blocks/checkbox-list-expandable/checkbox-list-expandable.js';
+import '../../blocks/dropdown/dropdown.scss';
+import '../../blocks/dropdown/dropdown.ts';
+import '../../blocks/footer/footer.scss';
+import '../../blocks/header/header.scss';
+import '../../blocks/input-text/input-text.scss';
+import '../../blocks/link/link.scss';
+import '../../blocks/pagination/pagination.scss';
+import '../../blocks/range-slider/range-slider.scss';
+import '../../blocks/rate-btn/rate-btn.scss';
+import '../../blocks/rate-btn/rate-btn.js';
+import '../../blocks/topic-label/topic-label.scss';
 
+import '../../blocks/calendar/calendar.scss';
+import '../../blocks/calendar/calendar.js';
+import '../../blocks/card-room/card-room.scss';
+import '../../blocks/card-room/card-room.js';
 
 /*pages*/
-import '../../pages/search-room/search-room.scss'
-import '../../pages/search-room/range-slider.js'
+import '../../pages/search-room/search-room.scss';
+import '../../pages/search-room/range-slider.js';
--- src/pages/sign-in/sign-in.js
+++ src/pages/sign-in/sign-in.js
@@ -1,16 +1,15 @@
-import '../../font/fonts.css'
-import '../../index.scss'
-
+import '../../font/fonts.css';
+import '../../index.scss';
 
 /*blocks*/
-import '../../blocks/btn/btn.scss'
-import '../../blocks/footer/footer.scss'
-import '../../blocks/form-sign-in/form-sign-in.scss'
-import '../../blocks/header/header.scss'
-import '../../blocks/input-text/input-text.scss'
-import '../../blocks/link/link.scss'
-import '../../blocks/topic-label/topic-label.scss'
+import '../../blocks/btn/btn.scss';
+import '../../blocks/input-text/input-text.scss';
+import '../../blocks/link/link.scss';
+import '../../blocks/topic-label/topic-label.scss';
 
+import '../../blocks/footer/footer.scss';
+import '../../blocks/header/header.scss';
+import '../../blocks/form-sign-in/form-sign-in.scss';
 
 /*pages*/
-import '../../pages/sign-in/sign-in.scss'
+import '../../pages/sign-in/sign-in.scss';
