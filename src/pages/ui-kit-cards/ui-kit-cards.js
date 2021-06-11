ddf43741684926e084360f12e5809ba00086b355
KsuVla <ksu.xa.v.ya@gmail.com>
2021-06-11 20:34:44 +0700

    fixed ui-kit-cards by styles (#5)

--- src/pages/ui-kit-cards/ui-kit-cards.js
+++ src/pages/ui-kit-cards/ui-kit-cards.js
@@ -1,31 +1,30 @@
-import '../../font/fonts.css'
-import '../../index.scss'
-
+import '../../font/fonts.css';
+import '../../index.scss';
 
 /*blocks*/
-import '../../blocks/btn/btn.scss'
-import '../../blocks/calendar/calendar.scss'
-import '../../blocks/calendar/calendar.js'
-import '../../blocks/card-room/card-room.scss'
-import '../../blocks/card-room/card-room.js'
-import '../../blocks/dropdown/dropdown.scss'
-import '../../blocks/dropdown/dropdown.ts'
-import '../../blocks/footer/footer.scss'
-import '../../blocks/form-registration/form-registration.scss'
-import '../../blocks/form-reservation/form-reservation.scss'
-import '../../blocks/form-search-num/form-search-num.scss'
-import '../../blocks/form-sign-in/form-sign-in.scss'
-import '../../blocks/header/header.scss'
-import '../../blocks/input-text/input-text.scss'
-import '../../blocks/input-text/input-text.js'
-import '../../blocks/link/link.scss'
-import '../../blocks/radiogroup/radiogroup.scss'
-import '../../blocks/rate-btn/rate-btn.scss'
-import '../../blocks/rate-btn/rate-btn.js'
-import '../../blocks/toggle-block/toggle-block.scss'
-import '../../blocks/toggle-block/toggle-block.js'
-import '../../blocks/topic-label/topic-label.scss'
+import '../../blocks/btn/btn.scss';
+import '../../blocks/dropdown/dropdown.scss';
+import '../../blocks/dropdown/dropdown.ts';
+import '../../blocks/footer/footer.scss';
+import '../../blocks/header/header.scss';
+import '../../blocks/input-text/input-text.scss';
+import '../../blocks/input-text/input-text.js';
+import '../../blocks/link/link.scss';
+import '../../blocks/radiogroup/radiogroup.scss';
+import '../../blocks/rate-btn/rate-btn.scss';
+import '../../blocks/rate-btn/rate-btn.js';
+import '../../blocks/toggle-block/toggle-block.scss';
+import '../../blocks/toggle-block/toggle-block.js';
+import '../../blocks/topic-label/topic-label.scss';
 
+import '../../blocks/calendar/calendar.scss';
+import '../../blocks/calendar/calendar.js';
+import '../../blocks/card-room/card-room.scss';
+import '../../blocks/card-room/card-room.js';
+import '../../blocks/form-registration/form-registration.scss';
+import '../../blocks/form-reservation/form-reservation.scss';
+import '../../blocks/form-search-num/form-search-num.scss';
+import '../../blocks/form-sign-in/form-sign-in.scss';
 
 /*pages*/
-import '../../pages/ui-kit-cards/ui-kit-cards.scss'
+import '../../pages/ui-kit-cards/ui-kit-cards.scss';
