fdc41c506b71b512d25999d9d4eaee2dfea692f1
KsuVla <ksu.xa.v.ya@gmail.com>
2021-06-12 02:32:42 +0700

    fixed fonts 'bold'; del excess files (#5)

--- src/pages/landing-page/landing-page.js
+++ src/pages/landing-page/landing-page.js
@@ -6,7 +6,6 @@ import '../../blocks/btn/btn.scss';
 import '../../blocks/dropdown/dropdown.scss';
 import '../../blocks/dropdown/dropdown.ts';
 import '../../blocks/input-text/input-text.scss';
-import '../../blocks/input-text/input-text.js';
 import '../../blocks/link/link.scss';
 import '../../blocks/topic-label/topic-label.scss';
 
