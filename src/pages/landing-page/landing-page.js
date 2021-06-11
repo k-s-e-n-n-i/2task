64721fed1d9702607fb5bf2f9a7bbf4230fa843b
KsuVla <ksu.xa.v.ya@gmail.com>
2021-06-11 20:46:01 +0700

    fixed landing-page by styles (#5)

--- src/pages/landing-page/landing-page.js
+++ src/pages/landing-page/landing-page.js
@@ -1,21 +1,20 @@
-import '../../font/fonts.css'
-import '../../index.scss'
-
+import '../../font/fonts.css';
+import '../../index.scss';
 
 /*blocks*/
-import '../../blocks/btn/btn.scss'
-import '../../blocks/calendar/calendar.scss'
-import '../../blocks/calendar/calendar.js'
-import '../../blocks/dropdown/dropdown.scss'
-import '../../blocks/dropdown/dropdown.ts'
-import '../../blocks/footer/footer.scss'
-import '../../blocks/form-search-num/form-search-num.scss'
-import '../../blocks/header/header.scss'
-import '../../blocks/input-text/input-text.scss'
-import '../../blocks/input-text/input-text.js'
-import '../../blocks/link/link.scss'
-import '../../blocks/topic-label/topic-label.scss'
+import '../../blocks/btn/btn.scss';
+import '../../blocks/dropdown/dropdown.scss';
+import '../../blocks/dropdown/dropdown.ts';
+import '../../blocks/input-text/input-text.scss';
+import '../../blocks/input-text/input-text.js';
+import '../../blocks/link/link.scss';
+import '../../blocks/topic-label/topic-label.scss';
 
+import '../../blocks/calendar/calendar.scss';
+import '../../blocks/calendar/calendar.js';
+import '../../blocks/form-search-num/form-search-num.scss';
+import '../../blocks/footer/footer.scss';
+import '../../blocks/header/header.scss';
 
 /*pages*/
-import '../../pages/landing-page/landing-page.scss'
+import '../../pages/landing-page/landing-page.scss';
--- src/pages/ui-kit-form-elements/ui-kit-form-elements.js
+++ src/pages/ui-kit-form-elements/ui-kit-form-elements.js
@@ -1,34 +1,33 @@
-import '../../font/fonts.css'
-import '../../index.scss'
-
+import '../../font/fonts.css';
+import '../../index.scss';
 
 /*blocks*/
-import '../../blocks/btn/btn.scss'
-import '../../blocks/calendar/calendar.scss'
-import '../../blocks/calendar/calendar.js'
-import '../../blocks/checkbox-list/checkbox-list.scss'
-import '../../blocks/checkbox-list-expandable/checkbox-list-expandable.scss'
-import '../../blocks/checkbox-list-expandable/checkbox-list-expandable.js'
-import '../../blocks/comment-block/comment-block.scss'
-import '../../blocks/dropdown/dropdown.scss'
-import '../../blocks/dropdown/dropdown.ts'
-import '../../blocks/info-block/info-block.scss'
-import '../../blocks/input-text/input-text.scss'
-import '../../blocks/input-text/input-text.js'
-import '../../blocks/like/like.scss'
-import '../../blocks/like/like.js'
-import '../../blocks/link/link.scss'
-import '../../blocks/list-block/list-block.scss'
-import '../../blocks/pagination/pagination.scss'
-import '../../blocks/radiogroup/radiogroup.scss'
-import '../../blocks/range-slider/range-slider.scss'
-import '../../blocks/rate-btn/rate-btn.scss'
-import '../../blocks/rate-btn/rate-btn.js'
-import '../../blocks/toggle-block/toggle-block.scss'
-import '../../blocks/toggle-block/toggle-block.js'
-import '../../blocks/topic-label/topic-label.scss'
+import '../../blocks/btn/btn.scss';
+import '../../blocks/checkbox-list/checkbox-list.scss';
+import '../../blocks/checkbox-list-expandable/checkbox-list-expandable.scss';
+import '../../blocks/checkbox-list-expandable/checkbox-list-expandable.js';
+import '../../blocks/comment-block/comment-block.scss';
+import '../../blocks/dropdown/dropdown.scss';
+import '../../blocks/dropdown/dropdown.ts';
+import '../../blocks/info-block/info-block.scss';
+import '../../blocks/input-text/input-text.scss';
+import '../../blocks/input-text/input-text.js';
+import '../../blocks/like/like.scss';
+import '../../blocks/like/like.js';
+import '../../blocks/link/link.scss';
+import '../../blocks/list-block/list-block.scss';
+import '../../blocks/pagination/pagination.scss';
+import '../../blocks/radiogroup/radiogroup.scss';
+import '../../blocks/range-slider/range-slider.scss';
+import '../../blocks/rate-btn/rate-btn.scss';
+import '../../blocks/rate-btn/rate-btn.js';
+import '../../blocks/toggle-block/toggle-block.scss';
+import '../../blocks/toggle-block/toggle-block.js';
+import '../../blocks/topic-label/topic-label.scss';
 
+import '../../blocks/calendar/calendar.scss';
+import '../../blocks/calendar/calendar.js';
 
 /*pages*/
-import '../../pages/ui-kit-form-elements/ui-kit-form-elements.scss'
-import '../../pages/ui-kit-form-elements/range-slider.js'
+import '../../pages/ui-kit-form-elements/ui-kit-form-elements.scss';
+import '../../pages/ui-kit-form-elements/range-slider.js';
