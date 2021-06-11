b7e6344c87b0dfdb715edb082060581dfaeba267
KsuVla <ksu.xa.v.ya@gmail.com>
2021-06-11 21:07:50 +0700

    fixed registration by styles (#5)

--- src/pages/registration/registration.js
+++ src/pages/registration/registration.js
@@ -1,19 +1,19 @@
-import '../../font/fonts.css'
-import '../../index.scss'
+import '../../font/fonts.css';
+import '../../index.scss';
 
 /*blocks*/
-import '../../blocks/btn/btn.scss'
-import '../../blocks/footer/footer.scss'
-import '../../blocks/form-registration/form-registration.scss'
-import '../../blocks/header/header.scss'
-import '../../blocks/input-text/input-text.scss'
-import '../../blocks/input-text/input-text.js'
-import '../../blocks/link/link.scss'
-import '../../blocks/radiogroup/radiogroup.scss'
-import '../../blocks/toggle-block/toggle-block.scss'
-import '../../blocks/toggle-block/toggle-block.js'
-import '../../blocks/topic-label/topic-label.scss'
+import '../../blocks/btn/btn.scss';
+import '../../blocks/input-text/input-text.scss';
+import '../../blocks/input-text/input-text.js';
+import '../../blocks/link/link.scss';
+import '../../blocks/radiogroup/radiogroup.scss';
+import '../../blocks/toggle-block/toggle-block.scss';
+import '../../blocks/toggle-block/toggle-block.js';
+import '../../blocks/topic-label/topic-label.scss';
 
+import '../../blocks/footer/footer.scss';
+import '../../blocks/header/header.scss';
+import '../../blocks/form-registration/form-registration.scss';
 
 /*pages*/
-import '../../pages/registration/registration.scss'
+import '../../pages/registration/registration.scss';
