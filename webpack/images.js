c64230d922626ba2686a18c1d5413d3be93553a7
KsuVla <ksu.xa.v.ya@gmail.com>
2021-06-23 22:41:33 +0700

    del comments (#11)

--- webpack/images.js
+++ webpack/images.js
@@ -1,23 +1,12 @@
-module.exports = function() {
-	return {
-		module: {
-			rules: [
-				{
-					test: /\.(png|svg|jpg|jpeg|gif)$/i,
-					//type: 'asset/resource',
-					loader: 'file-loader',
-										
-					
-					/*options: {	
-						name: 'images/[name].[contenthash].[ext]'
-					}
-					/*use: [
-            'url-loader', 
-            'html-loader',
-            'file-loader',
-          ]*/
-				},
-			]
-		}
-	};
+module.exports = function () {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          loader: 'file-loader',
+        },
+      ],
+    },
+  };
 };
