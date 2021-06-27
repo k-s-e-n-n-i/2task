83642a170d7e7279ec91f4a371c3dee9061e016c
KsuVla <ksu.xa.v.ya@gmail.com>
2021-06-27 22:16:36 +0700

    del excess imports

--- src/pages/ui-kit-cards/ui-kit-cards.js
+++ src/pages/ui-kit-cards/ui-kit-cards.js
@@ -2,9 +2,6 @@
 import '../../blocks/btn/btn.scss';
 import '../../blocks/dropdown/dropdown.scss';
 import '../../blocks/dropdown/dropdown.ts';
-import '../../blocks/footer/footer.scss';
-import '../../blocks/header/header.scss';
-import '../../blocks/header/header.ts';
 import '../../blocks/input-text/input-text.scss';
 import '../../blocks/input-text/input-text.js';
 import '../../blocks/link/link.scss';
