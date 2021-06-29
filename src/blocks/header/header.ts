12ee19d6bf4e8086979ba870ed478cffb437f8c3
KsuVla <ksu.xa.v.ya@gmail.com>
2021-06-29 22:38:18 +0700

    fixed header (#17)

--- src/blocks/header/header.ts
+++ src/blocks/header/header.ts
@@ -2,16 +2,18 @@ $(function () {
   $('.header__menu-mobile').on('click', handleMenuMobileClick);
 
   function handleMenuMobileClick() {
-    let menu = $(this).closest('.header__links').find('.header__block-menu');
-    let menuIcon = $(this).closest('.header__links').find('.header__menu-mobile');
+    let menu = $(this).closest('.header__content-container').find('.header__links');
+    let menuIcon = $(this);
 
-    if (menu.hasClass('header__block-menu_mobile')) {
-      menu.removeClass('header__block-menu_mobile');
-      menu.addClass('header__block-menu_hidden');
+    console.log($(this), menu, menuIcon);
+
+    if (menu.hasClass('header__links_mobile')) {
+      menu.removeClass('header__links_mobile');
+      menu.addClass('header__links_hidden');
       menuIcon.removeClass('header__menu-mobile_cross');
     } else {
-      menu.addClass('header__block-menu_mobile');
-      menu.removeClass('header__block-menu_hidden');
+      menu.addClass('header__links_mobile');
+      menu.removeClass('header__links_hidden');
       menuIcon.addClass('header__menu-mobile_cross');
     }
   }
