8dd3aa5be163df68df03caddc52411125c96c42b
KsuVla <ksu.xa.v.ya@gmail.com>
2021-07-05 06:08:54 +0700

    add build fonts in ./font

--- webpack/font.js
+++ webpack/font.js
@@ -5,6 +5,9 @@ module.exports = function () {
         {
           test: /\.(woff|woff2|eot|ttf|otf)$/i,
           type: 'asset/resource',
+          generator: {
+            filename: 'fonts/[name][ext]',
+          },
         },
       ],
     },
