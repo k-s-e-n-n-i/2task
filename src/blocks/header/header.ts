56551129de112e7debe3019883303b42b182b1e3
KsuVla <ksu.xa.v.ya@gmail.com>
2021-07-06 03:31:26 +0700

    del console.log

--- src/blocks/header/header.ts
+++ src/blocks/header/header.ts
@@ -5,8 +5,6 @@ $(function () {
     let menu = $(this).closest('.header__content-container').find('.header__links');
     let menuIcon = $(this);
 
-    console.log($(this), menu, menuIcon);
-
     if (menu.hasClass('header__links_mobile')) {
       menu.removeClass('header__links_mobile');
       menu.addClass('header__links_hidden');
