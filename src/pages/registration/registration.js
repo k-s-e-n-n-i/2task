e99d2a51ac0a04229a55400d4257751e7569f709
KsuVla <ksu.xa.v.ya@gmail.com>
2021-07-14 17:04:50 +0700

    fixed media 'form-registration' (#17, comment 08.07.21 15:46)

--- src/pages/registration/registration.js
+++ src/pages/registration/registration.js
@@ -11,6 +11,5 @@ import '@Blocks/footer/footer.scss';
 import '@Blocks/header/header.scss';
 import '@Blocks/header/header.ts';
 import '@Blocks/form-registration/form-registration.scss';
-import '@Blocks/form-registration/form-registration-media.scss';
 
 import '@Pages/registration/registration.scss';
