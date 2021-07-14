779d4e6406885203f6eee6a92c929c83e6cb644f
KsuVla <ksu.xa.v.ya@gmail.com>
2021-07-14 15:23:12 +0700

    fixed 'header__submenu', hover -> handleSubmenuClick (#17)

--- src/blocks/header/header.ts
+++ src/blocks/header/header.ts
@@ -15,4 +15,16 @@ $(function () {
       menuIcon.addClass('header__menu-mobile_cross');
     }
   }
+
+  $('.header__menu-li_expand').on('click', handleSubmenuClick);
+
+  function handleSubmenuClick() {
+    let submenu = $(this).find('.header__submenu');
+
+    if (submenu.hasClass('header__submenu-hide')) {
+      submenu.removeClass('header__submenu-hide').addClass('header__submenu-open');
+    } else {
+      submenu.removeClass('header__submenu-open').addClass('header__submenu-hide');
+    }
+  }
 });
