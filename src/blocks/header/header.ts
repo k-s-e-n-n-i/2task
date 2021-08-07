5314db3199a9a04f45d762766f807d1f80112269
KsuVla <ksu.xa.v.ya@gmail.com>
2021-08-07 18:24:46 +0700

    'add/remove Class' changed to toggleClass (in 'handleMenuMobileClick', 'handleSubmenuClick') (#17)

--- src/blocks/header/header.ts
+++ src/blocks/header/header.ts
@@ -5,26 +5,13 @@ $(function () {
     let menu = $(this).closest('.header__content-container').find('.header__links');
     let menuIcon = $(this);
 
-    if (menu.hasClass('header__links_mobile')) {
-      menu.removeClass('header__links_mobile');
-      menu.addClass('header__links_hidden');
-      menuIcon.removeClass('header__menu-mobile_cross');
-    } else {
-      menu.addClass('header__links_mobile');
-      menu.removeClass('header__links_hidden');
-      menuIcon.addClass('header__menu-mobile_cross');
-    }
+    menu.toggleClass('header__links_mobile header__links_hidden');
+    menuIcon.toggleClass('header__menu-mobile_cross');
   }
 
   $('.header__menu-li_expand').on('click', handleSubmenuClick);
 
   function handleSubmenuClick() {
-    let submenu = $(this).find('.header__submenu');
-
-    if (submenu.hasClass('header__submenu-hide')) {
-      submenu.removeClass('header__submenu-hide').addClass('header__submenu-open');
-    } else {
-      submenu.removeClass('header__submenu-open').addClass('header__submenu-hide');
-    }
+    $(this).find('.header__submenu').toggleClass('header__submenu-open');
   }
 });
