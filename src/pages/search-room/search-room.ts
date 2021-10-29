2942d5d93931e8d05878e12d2bd763c430bc30e1
KsuVla <ksu.xa.v.ya@gmail.com>
2021-10-30 04:05:11 +0700

    declared instances Dropdown in 'search-room' (#23)

--- src/pages/search-room/search-room.ts
+++ src/pages/search-room/search-room.ts
@@ -23,3 +23,63 @@ import '@Blocks/header/header';
 
 import '@Pages/search-room/search-room.scss';
 import '@Pages/search-room/range-slider';
+
+import { Dropdown } from '../../blocks/dropdown/dropdown';
+
+new Dropdown({
+  dropdown: document.getElementById('dropdownSearchRoomGuests'),
+  hasButtons: true,
+  valueDefault: 'Сколько гостей',
+  maxSum: 10,
+  countSum: true,
+  wordsFormSum: ['гость', 'гостя', 'гостей'],
+  items: [
+    {
+      text: 'взрослые',
+      number: 2,
+      countAdditionally: false,
+      wordsForm: ['взрослый', 'взрослых', 'взрослых'],
+    },
+    {
+      text: 'дети',
+      number: 0,
+      countAdditionally: false,
+      wordsForm: ['ребенок', 'ребенка', 'детей'],
+    },
+    {
+      text: 'младенцы',
+      number: 1,
+      countAdditionally: true,
+      wordsForm: ['младенец', 'младенца', 'младенцев'],
+    },
+  ],
+});
+
+new Dropdown({
+  dropdown: document.getElementById('dropdownSearchRoomComfort'),
+  hasButtons: false,
+  valueDefault: 'Сколько гостей',
+  maxSum: 15,
+  countSum: false,
+  wordsFormSum: [],
+  items: [
+    {
+      text: 'спальни',
+      number: 2,
+      countAdditionally: false,
+      wordsForm: ['спальня', 'спальни', 'спален'],
+    },
+    {
+      text: 'кровати',
+      number: 2,
+      countAdditionally: false,
+      wordsForm: ['кровать', 'кровати', 'кроватей'],
+    },
+    {
+      text: 'ванные комнаты',
+      number: 0,
+      countAdditionally: true,
+      wordsForm: ['ванная комната', 'ванные комнаты', 'ванных комнат'],
+    },
+  ],
+});
